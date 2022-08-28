1c16b004d803549950cc1d97fa01c7b2151d4bd4
psomers <psvd3@umsystem.edu>
2022-08-28 19:57:22 +0200

    compiles and imports

--- src/python_bindings/VideoDisplay_bindings.cpp
+++ src/python_bindings/VideoDisplay_bindings.cpp
@@ -1,13 +1,15 @@
 #include <pybind11/pybind11.h>
 #include <pybind11/stl_bind.h>
-
+#include <QtWidgets>
 #include "VideoDisplay.hpp"
 
 
 namespace py = pybind11;
 
 void init_VideoDisplay(py::module & m){
-    py::class_<VideoPlayer>(m, "VideoPlayer")
-        .def(py::init<>());
+    py::class_<QWidget>(m, "QWidget");
+    py::class_<VideoPlayer, QWidget>(m, "VideoPlayer")
+        .def(py::init<>())
+        .def("set_video", &VideoPlayer::set_video);
 
 }
